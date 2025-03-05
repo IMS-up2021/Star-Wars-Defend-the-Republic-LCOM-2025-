ae5a1274b7ab476385a6d405fb1e1ea9a67169f8
zepilim <zepilim05@gmail.com>
2025-03-05 16:26:05 +0000

    Test - timer_test_int done and passed

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -1,6 +1,6 @@
 #include <lcom/lcf.h>
 #include <lcom/lab2.h>
-
+#include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
 
@@ -45,16 +45,44 @@ int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
 }
 
 int(timer_test_time_base)(uint8_t timer, uint32_t freq)   {
+  if(timer > 2 || freq < 19) return 1;
   return timer_set_frequency(timer, freq);
 }
 
 int(timer_test_int)(uint8_t time) {
+  int ipc_status, r;
+  uint8_t hook_id_timer;
+  message msg;
 
-  int error = timer_subscribe_int(time);
-  if(error) return error;
+  if(timer_subscribe_int(&hook_id_timer) != 0) return 1;
 
-  error = timer_unsubscribe_int();
-  if(error) return error;
+  while (time > 0) {
+    if( (r = driver_receive(ANY, &msg, &ipc_status)) != 0) {
+      printf("driver_receive failed with: %d", r);
+      continue;
+    }
+    if(is_ipc_notify(ipc_status)) {
+      switch (_ENDPOINT_P(msg.m_source))
+      {
+      case HARDWARE:
+
+        if(msg.m_notify.interrupts & hook_id_timer) {
+          timer_int_handler();
+          if(counter % 60 == 0) {
+            timer_print_elapsed_time();
+            time--;
+          }
+        }
+       
+        break;
+      
+      default:
+        break;
+      }
+    }
+  }
+
+  if(timer_unsubscribe_int() != 0) return 1;
 
   return 0;
 }
