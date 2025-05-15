a08eafad32f4060fd282711378d1a84c02cf90a9
zepilim <zepilim05@gmail.com>
2025-05-15 15:32:17 +0100

    mouse macros fixed

--- proj/src/controllers/keyboardMouse/mouse.c
+++ proj/src/controllers/keyboardMouse/mouse.c
@@ -1,12 +1,12 @@
 #include "mouse.h"
 #include "KBC.h"
+#include <lcom/lcf.h>
 
 struct packet mouse_packet;
 int mouse_hook_id = 4; // um valor qualquer [0..7], desde que seja diferente do teclado e do timer
 uint8_t byte_index = 0;
 uint8_t current_byte; // último byte a ser lido
 uint8_t mouse_bytes[3];
-
 
 // Subscrição das interrupções - Modo REENABLE para modo EXCLUSIVE
 int (mouse_subscribe_int)(uint8_t *bit_no) {
@@ -30,12 +30,11 @@ void (mouse_sync_bytes)() {
     if(byte_index == 0 && (current_byte & FIRST_BYTE)) {
         mouse_bytes[byte_index] = current_byte;
         byte_index++;
-    } 
+    }
     else if (byte_index > 0) {
         mouse_bytes[byte_index] = current_byte;
         byte_index++;
     }
-    
 }
 
 void (mouse_bytes_to_packet)() {
@@ -47,24 +46,23 @@ void (mouse_bytes_to_packet)() {
     mouse_packet.lb = mouse_bytes[0] & MOUSE_LB;
     mouse_packet.x_ov = mouse_bytes[0] & MOUSE_X_OVERFLOW;
     mouse_packet.y_ov = mouse_bytes[0] & MOUSE_Y_OVERFLOW;
-    mouse_packet.delta_x = (mouse_bytes[0] & MOUSE_X_SIGNAL) ? (0xFF00 | mouse_bytes[1]) : mouse_bytes[1];
-    mouse_packet.delta_y = (mouse_bytes[0] & MOUSE_Y_SIGNAL) ? (0xFF00 | mouse_bytes[2]) : mouse_bytes[2];
+    mouse_packet.delta_x = (mouse_bytes[0] & MOUSE_X_DELTA) ? (0xFF00 | mouse_bytes[1]) : mouse_bytes[1];
+    mouse_packet.delta_y = (mouse_bytes[0] & MOUSE_Y_DELTA) ? (0xFF00 | mouse_bytes[2]) : mouse_bytes[2];
 }
 
 // Em comparação com o kbd, a escrita do mouse tem de ser muito mais controlada, e a realização de um comando tem que ser feito diretamente e depois temos de esperar por uma resposta afirmativa
 int (mouse_write)(uint8_t command) {
-    
     uint8_t attemps = MAX_ATTEMPS;
     uint8_t mouse_response;
 
-    do{
+    do {
         attemps--;
-        if (write_kbc_cmd(KBC_IN_CMD, WRITE_BYTE_MOUSE)) return 1;
+        if (write_kbc_cmd(KBC_IN_CMD, MOUSE_CFG)) return 1;
         if (write_kbc_cmd(KBC_OUT_CMD, command)) return 1;
         tickdelay(micros_to_ticks(WAIT_KBC));
         if (util_sys_inb(KBC_OUT_CMD, &mouse_response)) return 1;
-        if (mouse_response == ACK) return 0;
-    } while (mouse_response != ACK && attemps);
+        if (mouse_response == MOUSE_ACK) return 0;
+    } while (mouse_response != MOUSE_ACK && attemps);
 
     return 1;
 }
