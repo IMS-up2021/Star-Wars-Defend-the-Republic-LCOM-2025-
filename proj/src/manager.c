274edf8537335074df3afc41ec088fdb9d179936
Inês Soares <ines.soares.22102003@gmail.com>
2025-05-15 11:00:02 +0100

    initialize_graphics and exit_game done but not tested

---
+++ proj/src/manager.c
@@ -0,0 +1,92 @@
+#include <sys/time.h>
+
+#include "manager.h"
+#include "draw.h"
+
+uint32_t timer_irq_set, kbd_irq_set, mouse_irq_set;
+
+int kbd_i;
+bool mouse_ready;
+int timer_counter;
+
+uint64_t delta_time;
+
+int (initialize_graphics)() {
+    uint8_t timer_bit_no, kbd_bit_no, mouse_bit_no;
+    if (timer_subscribe_int(&timer_bit_no) != 0) {
+        printf("Error subscribing timer interrupts\n");
+        return 1;
+    }
+    if (kbd_subscribe_int(&kbd_bit_no) != 0) {
+        printf("Error subscribing keyboard interrupts\n");
+        timer_unsubscribe_int();
+        return 1;
+    }
+    if (mouse_subscribe_int(&mouse_bit_no) != 0) {
+        printf("Error subscribing mouse interrupts\n");
+        kbd_unsubscribe_int();
+        timer_unsubscribe_int();
+        return 1;
+    }
+
+    if (mouse_enable_data_reporting() != 0) {
+        printf("Error enabling mouse data reporting\n");
+        mouse_unsubscribe_int();
+        kbd_unsubscribe_int();
+        timer_unsubscribe_int();
+        return 1;
+    }
+
+    timer_irq_set = BIT(timer_bit_no);
+    kbd_irq_set = BIT(kbd_bit_no);
+    mouse_irq_set = BIT(mouse_bit_no);
+
+    if (vg_init() == NULL) {
+        printf("Error initializing graphics mode\n");
+        mouse_unsubscribe_int();
+        kbd_unsubscribe_int();
+        timer_unsubscribe_int();
+        return 1;
+    }
+
+    if (load_xpms() != 0) {
+        printf("Error loading XPMs\n");
+        mouse_unsubscribe_int();
+        kbd_unsubscribe_int();
+        timer_unsubscribe_int();
+        return 1;
+    }
+
+    init_background_menu();
+    init_buttons();
+    init_cursor();
+    init_walls();
+    init_players();
+    init_background_game_over();
+    init_background_instructions();
+    init_background();
+
+    return 0;
+}
+
+int (exit_game)() {
+    if (mouse_unsubscribe_int() != 0) {
+        printf("Error unsubscribing mouse interrupts\n");
+        return 1;
+    }
+    if (kbd_unsubscribe_int() != 0) {
+        printf("Error unsubscribing keyboard interrupts\n");
+        return 1;
+    }
+    if (timer_unsubscribe_int() != 0) {
+        printf("Error unsubscribing timer interrupts\n");
+        return 1;
+    }
+    if (mouse_enable_data_reporting() != 0) {
+        printf("Error disabling mouse data reporting\n");
+        return 1;
+    }
+    vg_exit();
+
+    return 0;
+}
---
+++ proj/src/manager.h
@@ -0,0 +1,31 @@
+#ifndef MANAGER_H
+#define MANAGER_H
+
+#include <lcom/lcf.h>
+#include "controllers/timer/timer.h"
+#include "controllers/keyboardMouse/keyboard.h"
+#include "controllers/keyboardMouse/mouse.h"
+#include "controllers/video/graphics.h"
+
+extern uint32_t timer_irq_set;
+extern uint32_t kbd_irq_set;
+extern uint32_t mouse_irq_set;
+extern void *frame_buffer;
+extern void *second_buffer;
+extern uint16_t center_x;
+extern uint16_t center_y;
+extern uint64_t delta_time;
+
+int (initialize_graphics)();
+
+int (load_menu_assets)();
+
+int (game_loop)();
+
+int (exit_game)();
+void (load_xpms)();
+void (handle_timer_event)();
+void (handle_kbd_event)(uint8_t scancode[]);
+void (handle_mouse_event)(struct packet pp);
+
+#endif // MANAGER_H
