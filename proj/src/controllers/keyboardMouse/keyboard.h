b70127c11188308a251d19582bcd7f3977c05c00
Inês Soares <ines.soares.22102003@gmail.com>
2025-05-28 13:36:46 +0100

    kbd started

--- proj/src/controllers/keyboardMouse/keyboard.h
+++ proj/src/controllers/keyboardMouse/keyboard.h
@@ -5,23 +5,14 @@
 #include <lcom/lcf.h>
 #include "i8042.h"
 
-/**
- * @brief Subscribes and enables Keyboard interrupts
- * @param bit_no Address of memory to be initialized with the bit number to be set in the mask returned upon an interrupt
- * @return 0 if successful, 1 otherwise
- */
+extern uint8_t scancode[2];
+
 int kbd_subscribe_int(uint8_t *bit_no);
 
-/**
- * @brief Unsubscribes Keyboard interrupts
- * @return 0 if successful, 1 otherwise
- */
 int kbd_unsubscribe_int();
 
 void (kbc_ih)();
 
 int(kbd_restore)();
-
-extern uint8_t scancode;
 
 #endif
--- proj/src/gameState.c
+++ proj/src/gameState.c
@@ -20,6 +20,8 @@ extern int timer_global_counter;
 
 struct packet mouse_packet; 
 
+int kbd_index;
+uint8_t scancode[2];
 
 gameState state = MAIN_MENU;
 
@@ -28,7 +30,7 @@ void gameLoop(void) {
     message msg;
     bool running = true;
     lcf_log_output("/home/lcom/labs/grupo_2leic18_2/proj/src/output.txt");
-
+    printf("Keyboard IRQ subscribed: irq_set=0x%X\n", kbd_irq_set);
     init_cursor();
 
     while (running) {
@@ -36,19 +38,27 @@ void gameLoop(void) {
 
         if (is_ipc_notify(ipc_status)) {
             if (_ENDPOINT_P(msg.m_source) == HARDWARE) {
+
                 if (msg.m_notify.interrupts & timer_irq_set) {
                     timer_int_handler();
                     if (timer_global_counter % 2 == 0) {
                         unsigned int _frame_size = mode_info.XResolution * mode_info.YResolution * ((mode_info.BitsPerPixel + 7) / 8);
                         memset(double_buffer, 0, _frame_size);
-
                         timer_event_handler(state);
-
                         draw_cursor(cursor);
-
                         vg_swap_buffers();
                     }
-                    
+                }
+                if (msg.m_notify.interrupts & kbd_irq_set) {
+                    kbc_ih();
+                    if (scancode[kbd_index] == TWO_BYTE_CODE) {
+                        kbd_index++;
+                        continue;
+                    }
+                    kbd_index = 0;
+                    // kbd_event_handler(scan_code);
+                    scancode[0] = 0;
+                    scancode[1] = 0;
                 }
                 if (msg.m_notify.interrupts & mouse_irq_set) {
                     mouse_ih();
